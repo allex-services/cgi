5931855b765b8ad5739d6568fd1c2c29d0b33b79
Veljko Popovic <veljko@888.rs>
2015-08-11 16:44:02 +0200

    this thing still seems as not worked out till its end ...

--- web_component/src/directives.js
+++ web_component/src/directives.js
@@ -2,11 +2,12 @@
   module.directive('allexCgiUpload', ['$compile', function ($compile) {
     ///- use UploadMixIn as parent scope ...
     ///- only most important attributes supported .. For more, visit https://github.com/danialfarid/ng-file-upload and add attributes to directive in markup ...
+
     return {
       'restrict': 'E',
       'replace' : true,
       'scope' : false,
-      'template': '<div data-ng-model="_ctrl.uploadFiles" data-ngf-drag-over-class="dragover" ngf-multiple="{{_ctrl.uploadSettings.multiple}}" ngf-allow-dir="{{_ctrl.uploadSettings.allowDir}}" data-ngf-change="_ctrl.uploadOnFileDropped($files, $event, $rejected)"></div>',
+      'template': '<button data-ng-model="_ctrl.uploadFiles" data-ngf-drag-over-class="dragover" ngf-multiple="{{_ctrl.uploadSettings.multiple}}" ngf-allow-dir="{{_ctrl.uploadSettings.allowDir}}" data-ngf-change="_ctrl.uploadOnFileDropped($files, $event, $rejected)">Upload files</button>',
       'link': function (scope, el) {
         var recompile = false;
         var s = scope._ctrl.uploadSettings;
